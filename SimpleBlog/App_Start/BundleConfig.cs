97f9009d9bdccb37e10878b837544d42cf4b9d9f
Nicolae Pascaneanu <pascaneanu@outlook.com>
2015-02-02 22:29:49 +0200

    Added styles and scripts files

---
+++ SimpleBlog/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Optimization;
+
+namespace SimpleBlog.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundle)
+        {
+            bundle.Add(new StyleBundle("~/admin/styles")
+                .Include("~/content/styles/boostrap.css")
+                .Include("~/content/styles/admin.css"));
+
+            bundle.Add(new StyleBundle("~/styles")
+                .Include("~/content/styles/bootstrap.css")
+                .Include("~/content/styles/site.css"));
+
+            bundle.Add(new ScriptBundle("~/admin/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+
+            bundle.Add(new ScriptBundle("~/scripts")
+                .Include("~/scripts/jquery-2.1.3.js")
+                .Include("~/scripts/jquery.validate.js")
+                .Include("~/scripts/jquery.validate.unobtrusive.js")
+                .Include("~/scripts/bootstrap.js"));
+        }
+
+    }
+}
